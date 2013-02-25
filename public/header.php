8eae36359b6abfff34d1237de3dbd75872b1399c
t2t2 <taavot@gmail.com>
2013-02-25 10:15:53 +0200

    placeholders, php header + session start

---
+++ public/header.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title></title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
+
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/normalize.min.css">
+		<link rel="stylesheet" href="css/main.css">
+
+		<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+		<![endif]-->
+
+		<div class="header-container">
+			<header class="wrapper clearfix">
+				<h1 class="title"><a href="index.html">Valimised</a></h1>
+				<div class="user">
+					Pole sisse loginud -- <a href="#">Logi sisse &rsaquo;</a>
+				</div>
+				<nav class="clearfix">
+					<ul>
+						<li><a href="kandidaadid.html">Kandidaadid</a></li>
+						<li><a href="tulemused.html">Tulemused</a></li>
+						<li><a href="haaleta.html">Hääleta</a></li>
+					</ul>
+				</nav>
+			</header>
+		</div>
--- public/tulemused.php
+++ public/tulemused.php
@@ -1,41 +1,4 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title></title>
-		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width">
-
-		<link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="css/normalize.min.css">
-		<link rel="stylesheet" href="css/main.css">
-
-		<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-	</head>
-	<body>
-		<!--[if lt IE 7]>
-			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-		<![endif]-->
-
-		<div class="header-container">
-			<header class="wrapper clearfix">
-				<h1 class="title"><a href="index.html">Valimised</a></h1>
-				<div class="user">
-					Pole sisse loginud -- <a href="#">Logi sisse &rsaquo;</a>
-				</div>
-				<nav class="clearfix">
-					<ul>
-						<li><a href="kandidaadid.html">Kandidaadid</a></li>
-						<li><a href="tulemused.html">Tulemused</a></li>
-						<li><a href="haaleta.html">Hääleta</a></li>
-					</ul>
-				</nav>
-			</header>
-		</div>
+<?php include "header.php"; ?>
 
 		<div class="main-container">
 			<div class="main wrapper clearfix">
@@ -44,6 +7,13 @@
 					<small class="attrib">Kaart <a href="http://commons.wikimedia.org/wiki/File:Estonia_location_map.svg">CC-by-sa Wikipedia</a></small>
 				</div>
 				<div class="row-right">
+					<form>
+						<label>Piirkond:</label>
+						<select id="filter-region" >
+							<option value="0">Kogu Eesti</option>
+
+						</select>
+					</form>
 					<table>
 						<thead>
 							<tr>
