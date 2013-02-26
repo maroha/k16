e78ff357bb6cd2c5d0329adf5e3dd27f5f047ec5
t2t2 <taavot@gmail.com>
2013-02-26 06:39:53 +0200

    tulemused: Filter form, move up

--- public/tulemused.php
+++ public/tulemused.php
@@ -2,18 +2,28 @@
 
 		<div class="main-container">
 			<div class="main wrapper clearfix">
+				<form>
+					<label for="filter-region">Piirkond:</label>
+					<select id="filter-region" name="region">
+						<option value="0">Kogu Eesti</option>
+						<option value="1">Valimisringkond 1</option>
+						<option value="2">Valimisringkond 2</option>
+					</select>
+					<label for="filter-party">Partei:</label>
+					<select id="filter-party" name="party">
+						<option value="0">Kõik Parteid</option>
+						<option value="1">Partei 1</option>
+						<option value="2">Partei 2</option>
+					</select>
+					<label><input type="radio" name="type" value="party" checked="checked" /> Partei</label>
+					<label><input type="radio" name="type" value="person" /> Isik</label>
+					<button type="submit">Värskenda</button>
+				</form>
 				<div class="row-left">
 					<img src="img/map.png" alt="Kaart" />
 					<small class="attrib">Kaart <a href="http://commons.wikimedia.org/wiki/File:Estonia_location_map.svg">CC-by-sa Wikipedia</a></small>
 				</div>
 				<div class="row-right">
-					<form>
-						<label>Piirkond:</label>
-						<select id="filter-region" >
-							<option value="0">Kogu Eesti</option>
-
-						</select>
-					</form>
 					<table>
 						<thead>
 							<tr>
