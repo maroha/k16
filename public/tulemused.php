0546b0518881c4573c3d36c57f6ab87ca8c3bb68
ekremm <jevgeni.kremm@gmail.com>
2013-03-10 16:40:06 +0200

    Tulemuste tabeli sorteerimine

--- public/tulemused.php
+++ public/tulemused.php
@@ -30,7 +30,7 @@
 							<small class="attrib">Kaart <a href="http://commons.wikimedia.org/wiki/File:Estonia_location_map.svg">CC-by-sa Wikipedia</a></small>
 						</div>
 						<div class="row-right">
-							<table>
+							<table class="sortable">
 								<thead>
 									<tr>
 										<th>Partei</th>
@@ -39,35 +39,35 @@
 								</thead>
 								<tbody>
 									<tr>
-										<th>Partei</th>
+										<th>Partei A</th>
 										<td>
 											<div class="result-row" style="width: 50%;"></div>
 											<div class="result-text">500,000 (25%)</div>
 										</td>
 									</tr>
 									<tr>
-										<th>Partei</th>
+										<th>Partei B</th>
 										<td>
 											<div class="result-row" style="width: 40%;"></div>
 											<div class="result-text">400,000 (20%)</div>
 										</td>
 									</tr>
 									<tr>
-										<th>Partei</th>
+										<th>Partei C</th>
 										<td>
 											<div class="result-row" style="width: 30%;"></div>
 											<div class="result-text">300,000 (15%)</div>
 										</td>
 									</tr>
 									<tr>
-										<th>Partei</th>
+										<th>Partei D</th>
 										<td>
 											<div class="result-row" style="width: 20%;"></div>
 											<div class="result-text">200,000 (10%)</div>
 										</td>
 									</tr>
 									<tr>
-										<th>Partei</th>
+										<th>Partei E</th>
 										<td>
 											<div class="result-row" style="width: 10%;"></div>
 											<div class="result-text">100,000 (5%)</div>
