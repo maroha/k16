be70bcb01e47003f0e11d2b52c01d155aa25c8b6
t2t2 <taavot@gmail.com>
2013-03-20 15:14:53 +0200

    Stupid simple migrating #DEVELOPERS #DEVELOPERS #DEVELOPERS

---
+++ migrate.bat
@@ -0,0 +1,13 @@
+@echo off
+rem Dumb-proof migrating, expects this to be installed in xampp/htdocs/k16 (exact folder names are not required)
+
+echo "> Trying global"
+phpx artisan migrate --env=local
+
+if ERRORLEVEL 1 (
+	echo "> Fallback: Trying relative"
+	..\..\php\php artisan migrate --env=local
+)
+if ERRORLEVEL 1 (
+	echo "Sorry bro, but I have no idea where your php is. (Try adding it to PATH)"
+)
