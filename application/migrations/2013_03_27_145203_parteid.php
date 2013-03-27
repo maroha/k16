c4a04760bc8c0dfd1f2cf730c9e7d1546a6335d7
ekremm <jevgeni.kremm@gmail.com>
2013-03-27 16:58:15 +0200

    Parteid SQL

---
+++ application/migrations/2013_03_27_145203_parteid.php
@@ -0,0 +1,26 @@
+<?php
+
+class Parteid {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$sql = "INSERT INTO `partei` (`ID`, `Nimetus`) VALUES (NULL, 'Partei 1'), (NULL, 'Partei 2'), (NULL, 'Partei 3'), (NULL, 'Partei 4'), (NULL, 'Partei 5'), (NULL, 'Partei 6'), (NULL, 'Partei 7'), (NULL, 'Partei 8'), (NULL, 'Partei 9'), (NULL, 'Partei 10');";
+		DB::query($sql);
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::query("DELETE FROM `partei ;");
+	}
+
+}
