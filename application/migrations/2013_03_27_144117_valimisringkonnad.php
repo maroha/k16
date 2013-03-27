74bfa13d4c7f66cbfad301decc4c46d5e1894f26
ekremm <jevgeni.kremm@gmail.com>
2013-03-27 16:43:44 +0200

    Valimisringkonnad SQL

---
+++ application/migrations/2013_03_27_144117_valimisringkonnad.php
@@ -0,0 +1,26 @@
+<?php
+
+class Valimisringkonnad {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$sql = "INSERT INTO `db`.`valimisringkond` (`ID`, `Nimetus`) VALUES (NULL, \'Valimisringkond 1\'), (NULL, \'Valimisringkond 2\'), (NULL, \'Valimisringkond 3\'), (NULL, \'Valimisringkond 4\'), (NULL, \'Valimisringkond 5\'), (NULL, \'Valimisringkond 6\'), (NULL, \'Valimisringkond 7\'), (NULL, \'Valimisringkond 8\'), (NULL, \'Valimisringkond 9\'), (NULL, \'Valimisringkond 10\');";
+		DB::query($sql);
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
