475b98fc100b0a58ab1f2de0f989722b5b19312d
ekremm <jevgeni.kremm@gmail.com>
2013-03-30 13:06:34 +0200

    Haaled SQL

---
+++ application/migrations/2013_03_30_080021_haaled.php
@@ -0,0 +1,26 @@
+<?php
+
+class Haaled {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$sql = "INSERT INTO `haal` (`Aeg`, `Haaletaja_ID`, `Kandidaadi_ID`) VALUES ('2000-01-01 00:01:00', 1, 2), ('2000-01-01 00:02:00', 2, 2), ('2000-01-01 00:03:00', 3, 2), ('2000-01-01 00:04:00', 4, 2), ('2000-01-01 00:05:00', 5, 2), ('2000-01-01 00:06:00', 6, 2), ('2000-01-01 00:07:00', 7, 2), ('2000-01-01 00:08:00', 8, 2), ('2000-01-01 00:09:00', 9, 2), ('2000-01-01 00:10:00', 9, 2);";
+		DB::query($sql);
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::query("DELETE FROM `valimisringkond`;");
+	}
+
+}
