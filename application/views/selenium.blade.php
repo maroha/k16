2651b8a301598cbb321dd9836003ab9a719767b5
t2t2 <taavot@gmail.com>
2013-04-30 21:53:07 +0300

    puhastustalgud

--- application/views/selenium.blade.php
+++ application/views/selenium.blade.php
@@ -116,35 +116,3 @@
 	<script src="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
-
-<?php /*
-<h1>Results!</h1>
-@foreach($results as $id => $result)
-	<h2>{{{ $tests[$id]["name"] }}}</h2>
-	@if($result["success"])
-		<p>Test success!</p>
-		<pre>{{ $result["log"] }}</pre>
-	@else
-		<p>Test failed</p>
-		<pre>{{ $result["log"] }}</pre>
-		<h3>{{ $result["error"]->getMessage() }}</h3>
-		@if(isset($result["error"]->data["screenshot"]))
-			<p>Screenshot:</p>
-			<img src="data:image/png;base64,{{ $exception_results["value"]["screen"] }}" />
-		@endif
-		<pre>{{ var_dump(array_except($result["error"]->data, array("screenshot"))) }}</pre>
-		@if($result["error"]->getPrevious())
-			@if($result["error"]->getPrevious() instanceof WebDriverException)
-				<?php $exception_results = $result["error"]->getPrevious()->getResults() ?>
-				<h3>{{ $exception_results["state"] }}</h3>
-				<p>Message:</p>
-				{{ $exception_results["value"]["message"] }}
-				<p>Screenshot:</p>
-				<img src="data:image/png;base64,{{ $exception_results["value"]["screen"] }}" />
-			@else
-				<h3>{{ $result["error"]->getPrevious()->getMessage() }}</h3>
-			@endif
-		@endif
-	@endif
-@endforeach
-*/
